deb73c11e224a415ea451ee99b83f9d55a3d0028
Johnson VILAYVANH <johnsonv@hotmail.fr>
2022-04-28 23:01:48 +0200

    Add stele

---
+++ include/stele.h
@@ -0,0 +1,31 @@
+#ifndef __STELE_H__
+#define __STELE_H__
+
+#include <g3x.h>
+#include "cube.h"
+#include "cylinder.h"
+#include "floor.h"
+#include "torus.h"
+#include "node.h"
+
+static double const PEDESTAL_X = 0.5;
+static double const PEDESTAL_Y = FLOOR_X;
+static double const PEDESTAL_Z = PEDESTAL_X;
+
+static double const TOWER_X = PEDESTAL_X * 0.8;
+static double const TOWER_Y = TOWER_X;
+static double const TOWER_Z = 3;
+
+static double const TOWER_RING_X = TOWER_X;
+static double const TOWER_RING_Y = TOWER_RING_X;
+static double const TOWER_RING_Z = TOWER_RING_X;
+
+static double pedestal_length   = PEDESTAL_X * CUBE_W;
+static double pedestal_width    = PEDESTAL_Y * CUBE_W;
+static double pedestal_height   = PEDESTAL_Z * CUBE_W;
+static double tower_height      = TOWER_Z * CYLINDER_H;
+static double tower_ring_height = TOWER_RING_Z * 0.2;
+
+int init_stele(SceneTree *stele, Shape *cube, Shape *cylinder, Shape *torus);
+
+#endif
---
+++ src/stele.c
@@ -0,0 +1,50 @@
+#include "../include/stele.h"
+
+int init_pedestal(SceneTree *stele, Shape *cube)
+{
+    if (!add_child(stele)) { return 0; }
+    (*stele)->down->instance     = cube;
+    (*stele)->down->col          = grey;
+    (*stele)->down->scale_factor = (G3Xvector) { PEDESTAL_X, PEDESTAL_Y, PEDESTAL_Z };
+    translate(&(*stele)->down, -pedestal_length / 2., -pedestal_width / 2., 0);
+    return 1;
+}
+
+int init_tower(SceneTree *stele, SceneTree *next, Shape *cylinder)
+{
+    if (!add_next(next, stele)) { return 0; }
+    (*next)->next->instance     = cylinder;
+    (*next)->next->col          = grey;
+    (*next)->next->scale_factor = (G3Xvector) { TOWER_X, TOWER_Y, TOWER_Z };
+    translate(&(*next)->next, 0, 0, pedestal_height + tower_height / 2.);
+
+    return 1;
+}
+
+int init_tower_ring(SceneTree *stele, SceneTree *next, Shape *torus)
+{
+    if (!add_next(next, stele)) { return 0; }
+    (*next)->next->instance     = torus;
+    (*next)->next->col          = grey;
+    (*next)->next->scale_factor = (G3Xvector) { TOWER_RING_X, TOWER_RING_Y, TOWER_RING_Z };
+    translate(&(*next)->next, 0, 0, pedestal_height + tower_ring_height / 2.);
+
+    return 1;
+}
+
+int init_stele(SceneTree *stele, Shape *cube, Shape *cylinder, Shape *torus)
+{
+    if (((*stele) = (Node *) malloc(sizeof(Node))) == NULL) { return 0; }
+    (*stele)->mat[0] = 0.25;
+    (*stele)->mat[1] = 0.5;
+    (*stele)->mat[2] = 0.0;
+    (*stele)->mat[3] = 1.;
+    (*stele)->Md = g3x_Identity();
+
+    return init_pedestal(stele, cube) &&
+           init_tower(stele, &(*stele)->down, cylinder) &&
+           init_tower_ring(stele, &(*stele)->down->next, torus);
+}
+
+
+
